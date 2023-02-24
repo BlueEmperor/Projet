4000d7a360527389624a76db10a298c55f7398af
BlueEmperor <pingoleon.arcus@gmail.com>
2023-02-24 23:18:36 +0100

    Update damage_effect.py

--- src/components/damage_effect.py
+++ src/components/damage_effect.py
@@ -12,8 +12,9 @@ class DamageEffect:
         self.font = VisualizationService.get_main_font()
         self.text = text
         self.position = position
-        self.velocity = random()+1
         self.angle = random()*2*pi/6+pi/3
+        rand=random()+2
+        self.velocity = vec(rand*cos(self.angle),-rand*sin(self.angle))
         self.timer = Config.FPS + int(random()*Config.FPS)
 
     def draw(self, SCREEN):
@@ -22,7 +23,8 @@ class DamageEffect:
         SCREEN.blit(text, self.position)
 
     def update(self):
-        self.position+=vec(self.velocity*cos(self.angle),-self.velocity*sin(self.angle))
+        self.position+=self.velocity
+        self.velocity[1]+=0.05
         self.timer-=1
         if(self.timer<=0):
             return(True)
